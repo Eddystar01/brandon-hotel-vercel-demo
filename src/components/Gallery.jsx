871e695bfa5eb686fb2c2a3a3a516b1350130bfa
Eddy Dev <olanrewajuolamide.o03@gmail.com>
2026-02-10 04:46:36 +0100

    edited gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,11 +1,11 @@
 import { motion } from "framer-motion";
 
 const images = [
-  "https://lh3.googleusercontent.com/gps-cs-s/AHVAwepBo4_efC6FkkFe9PcxUCVHX2BDFpAwF_0mUj9VkVm4k6Wpn4O3LiyTFDmdbE0xCA1huy4xHR7jL9NqL18EFIRTGRJ7qdcUhnPb2ga0xLSKxdlXwyUGuDgBlbKmtA6V9C01CRmNKpQs8b0=s1360-w1360-h1020-rw",
+  "https://lh3.googleusercontent.com/gps-cs-s/AHVAwepa9GsfpCiOk39HaaGGApSUiCKMaLYvmc-UQ_jEEghPRjykoJfYNzq8Sk91dk3tEfM5MWFp9SWpUYmhmJQndvtjUrnGAM8nDpRCd0Ba87oe8ykoteNne7nYo8HxcECV9q2ZczVXrsOc0F51=s1360-w1360-h1020-rw",
   "https://lh3.googleusercontent.com/gps-cs-s/AHVAwepa9GsfpCiOk39HaaGGApSUiCKMaLYvmc-UQ_jEEghPRjykoJfYNzq8Sk91dk3tEfM5MWFp9SWpUYmhmJQndvtjUrnGAM8nDpRCd0Ba87oe8ykoteNne7nYo8HxcECV9q2ZczVXrsOc0F51=w141-h118-n-k-no-nu",
   "https://images.unsplash.com/photo-1582719478250-c89cae4dc85b",
   "https://images.unsplash.com/photo-1540541338287-41700207dee6",
-  "https://lh3.googleusercontent.com/gps-cs-s/AHVAwepa9GsfpCiOk39HaaGGApSUiCKMaLYvmc-UQ_jEEghPRjykoJfYNzq8Sk91dk3tEfM5MWFp9SWpUYmhmJQndvtjUrnGAM8nDpRCd0Ba87oe8ykoteNne7nYo8HxcECV9q2ZczVXrsOc0F51=s1360-w1360-h1020-rw",
+  "https://images.unsplash.com/photo-1590490360182-c33d57733427",
   "https://images.unsplash.com/photo-1578683010236-d716f9a3f461",
 ];
 
